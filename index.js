bef3001db7365f27e37cf11d3d0c7381574ee40a
Be-bo <robfiker@gmail.com>
2023-11-17 11:24:24 -0800

    Database finished, all REST requests tested and working

--- index.js
+++ index.js
@@ -2,6 +2,7 @@
 const express = require('express');
 const app = express();
 const cors = require('cors');
+const fs = require('fs');
 const PORT = 80;
 const imgDir = 'public';
 const merchantsJsonPath = 'merchants.json';
@@ -21,22 +22,14 @@ app.get('/', (req, res) => res.send('Hello World!'));
 
 
 // MARK: Get Merchant Info
-app.get('/merchants/:merchantId', (req, res) => {
+app.get('/merchants/:merchantId', async (req, res) => {
     const merchantId = req.params.merchantId;
     try{
-        // TODO
-    }catch(error){
-        console.log(error);
-        res.status(500).json({error});
-    }
-});
-
-
-// MARK: Get Suggested Partnerships
-app.get('/partnerships/recommended/:merchantId', (req, res) => {
-    const merchantId = req.params.merchantId;
-    try{
-        // TODO
+        const merchantsJson = await fs.promises.readFile(merchantsJsonPath, 'utf-8');
+        const parsedMerchants = JSON.parse(merchantsJson);
+        const targetMerchant = parsedMerchants.filter((merchant) => merchant.merchant_id == merchantId);
+        console.log('Successfully returned GET merchant: ', targetMerchant[0]);
+        res.status(200).json(targetMerchant[0]);
     }catch(error){
         console.log(error);
         res.status(500).json({error});
@@ -45,9 +38,12 @@ app.get('/partnerships/recommended/:merchantId', (req, res) => {
 
 
 // MARK: Get Merchant Categories
-app.get('/categories', (req, res) => {
+app.get('/categories', async (req, res) => {
     try{
-        // TODO
+        const categoriesJson = await fs.promises.readFile(categoriesJsonPath, 'utf-8');
+        const parsedCategories = JSON.parse(categoriesJson);
+        console.log('Successfully returned list of categories: ', parsedCategories);
+        res.status(200).json(parsedCategories);
     }catch(error){
         console.log(error);
         res.status(500).json({error});
@@ -56,10 +52,31 @@ app.get('/categories', (req, res) => {
 
 
 // MARK: Get Current Partnerships
-app.get('/partnerships/:merchantId', (req, res) => {
+app.get('/partnerships/:merchantId', async (req, res) => {
     const merchantId = req.params.merchantId;
     try{
-        // TODO
+
+        const partnershipsJson = await fs.promises.readFile(partnershipsJsonPath, 'utf-8');
+        const parsedPartnerships = JSON.parse(partnershipsJson);
+        const merchantPartnerships = parsedPartnerships.filter((partnership) => partnership.merchant_ids.includes(parseInt(merchantId)));
+        console.log('Successfully returned partnerships for merchant id ', merchantId, ': ', merchantPartnerships);
+        res.status(200).json(merchantPartnerships);
+    }catch(error){
+        console.log(error);
+        res.status(500).json({error});
+    }
+});
+
+
+// MARK: Get Merchant Products
+app.get('/merchant/products/:merchantId', async (req, res) => {
+    const merchantId = req.params.merchantId;
+    try{
+        const productsJson = await fs.promises.readFile(productsJsonPath, 'utf-8');
+        const parsedProducts = JSON.parse(productsJson);
+        const productList = parsedProducts.filter((product) => product.merchant_id == merchantId);
+        console.log('Successfully returned product list for merchant ', merchantId, ': ', productList);
+        res.status(200).json(productList);
     }catch(error){
         console.log(error);
         res.status(500).json({error});
@@ -68,10 +85,16 @@ app.get('/partnerships/:merchantId', (req, res) => {
 
 
 // MARK: Post New Partnership
-app.post('/partnerships', (req, res) => {
-    const partnershipData = req.body.json;
+app.post('/partnerships', async (req, res) => {
+    let partnershipData = req.body;
     try{
-        // TODO
+        const partnershipsJson = await fs.promises.readFile(partnershipsJsonPath, 'utf-8');
+        let parsedPartnerships = JSON.parse(partnershipsJson);
+        partnershipData.partnership_id = parsedPartnerships.length+1;
+        parsedPartnerships.push(partnershipData);
+        await fs.promises.writeFile(partnershipsJsonPath, JSON.stringify(parsedPartnerships, null, 2));
+        console.log('Successfully posted new partnership: ', partnershipData);
+        res.status(200).json({message: 'Successfully added a new partnership.',});
     }catch(error){
         console.log(error);
         res.status(500).json({error});
